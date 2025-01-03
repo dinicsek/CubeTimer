fb78094843164195fc2f15c01338fcda6f114347
dinicsek <dinicsek01@gmail.com>
2025-01-03 13:28:29 +0100

    New Year, New Me

--- CubeTimer.WebApi/Infrastructure/Common/TimstampedEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CubeTimer.WebApi.Infrastructure.Common;
-
-public class TimestampedEntity
-{
-    public DateTime CreatedAt { get; set; }
-    
-    public DateTime UpdatedAt { get; set; }
-}
